5fa21c5d6ccb1de4eef0a168db70967667a4fc5f
aTerrariashitmodmaker <147291977+aTerrariashitmodmaker@users.noreply.github.com>
2024-10-06 19:50:54 +0800

    Add files via upload

---
+++ NPCs/友好蝙蝠.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Net;
+using Terraria;
+
+namespace 爆枪英雄.NPCs
+{
+    public class 友好蝙蝠 : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[NPC.type] = 4;
+            base.SetStaticDefaults();
+        }
+
+        public override void SetDefaults()
+        {
+            NPC.width = 30;
+            NPC.height = 20;
+            NPC.damage = 25;
+            NPC.defense = 100;
+            NPC.lifeMax = 800;
+            NPC.friendly = true;
+            //NPC.dontCountMe = true;
+            //NPC.netAlways = true;
+            NPC.HitSound = SoundID.NPCHit9;
+            NPC.DeathSound = SoundID.NPCDeath11;
+            NPC.noGravity = true;
+            NPC.noTileCollide = true;
+            NPC.knockBackResist = 0f;
+            //NPC.lavaImmune = true;
+            NPC.aiStyle = -1;
+            NPC.npcSlots = 0f;
+        }
+
+        float c = 0;
+        private void MoveAroundPlayer(Player player)
+        {
+            Vector2 diff = NPC.Center - player.Center;
+            float dis = diff.Length();
+            diff.Normalize();
+            //diff = diff.RotatedBy(MathHelper.PiOver2);
+            NPC.velocity -= diff * 0.2f;
+
+            if (NPC.Center.X < player.Center.X)
+            {
+                NPC.velocity.X += 0.1f;
+            }
+            if (NPC.Center.X > player.Center.X)
+            {
+                NPC.velocity.X -= 0.1f;
+            }
+            if (NPC.Center.Y < player.Center.Y)
+            {
+                NPC.velocity.Y += 0.1f;
+            }
+            if (NPC.Center.Y > player.Center.Y)
+            {
+                NPC.velocity.Y -= 0.1f;
+            }
+
+            if (dis > 80f)
+            {
+                if (Vector2.Dot(NPC.velocity, -diff) < 0.25f)
+                {
+                    NPC.velocity *= 0.9f;
+                }
+            }
+
+            if (NPC.velocity.Length() > 7.5f && dis < 200f)
+            {
+                NPC.velocity = Vector2.Normalize(NPC.velocity) * 7.5f;
+                NPC.velocity *= 0.98f;
+            }
+            if (Math.Abs(NPC.velocity.X) < 0.016f || Math.Abs(NPC.velocity.Y) < 0.016f)
+            {
+                NPC.velocity = Main.rand.NextVector2Circular(1, 1) * 2f;
+                NPC.velocity *= 1.1f;
+                NPC.netUpdate = true;
+            }
+        }
+        public override void AI()
+        {
+            //
+            if (c % 25 == 0)
+            {
+                Projectile.NewProjectile(NPC.GetSource_FromAI(), NPC.Center, Vector2.Zero, ProjectileType<蝙蝠接触伤害>(), NPC.damage, 6, default, NPC.whoAmI);
+            }
+            //
+            c++;
+            if (c % 10 == 0)
+            {
+                Dust dust = Main.dust[Dust.NewDust(NPC.Center, NPC.width, NPC.height, DustID.ShimmerTorch, 0, 0, 0, Color.HotPink)];
+            }
+            //
+            Player player = Main.player[(int)NPC.ai[0]];
+            if (Vector2.Distance(player.Center, NPC.Center) > 1500f)
+            {
+                NPC.Center = player.Center;
+                NPC.netUpdate = true;
+            }
+            //
+            if (player.dead)
+            {
+                NPC.life = -1;
+            }
+            //
+            int TarWho = -1;// 目标whoAmI
+            float misdis = 1500f;
+            for (int i = 0; i < 200; i++)
+            {
+                if (Main.npc[i].active && Main.npc[i].CanBeChasedBy(NPC, true) && (Main.npc[i].damage > 0))
+                {
+                    float dis = player.Distance(Main.npc[i].Center);
+                    if (dis < misdis)
+                    {
+                        misdis = dis;
+                        TarWho = Main.npc[i].whoAmI;
+                    }
+                }
+            }
+            //if (player.MinionAttackTargetNPC >= 0) target = Main.npc[player.MinionAttackTargetNPC];
+            //if (target != null && target.CanBeChasedBy())
+            //{
+            //    if (NPC.Distance(target.Center) < 1500f) TarWho = target.whoAmI;
+            //}
+            //if (TarWho < 0)
+            //{
+            //    foreach (NPC npc in Main.npc)
+            //    {
+            //        if ( (!npc.friendly)&&(npc.CanBeChasedBy()) && (player.Distance(npc.Center) < 1000f))
+            //        {
+            //            if (NPC.Distance(npc.Center) < 1000f)
+            //            {
+            //                TarWho = npc.whoAmI;
+            //            }
+            //        }
+            //    }
+            //}
+            //
+            if (TarWho != -1)
+            {
+                NPC npc = Main.npc[TarWho];
+                Vector2 tarVel = npc.Center - NPC.Center;
+                float speed = 0.4f;
+                float dis = tarVel.Length();
+                // 越近越快
+                if (dis < 600f)
+                {
+                    speed = 0.5f;
+                }
+                if (dis < 300f)
+                {
+                    speed = 0.7f;
+                }
+                if (dis > Math.Min(npc.Size.Length(), 80f))
+                {
+                    // 弹幕速度加上朝着npc的单位向量*追击速度系数*1.5f
+                    NPC.velocity += Vector2.Normalize(tarVel) * speed * 2f;
+                    //如果追踪方向和速度方向夹角过大，减速(向量点乘你们自己研究，绝对值越大两向量夹角越大，范围是正负1)
+                    if (Vector2.Dot(NPC.velocity, tarVel) < 0.25f)
+                    {
+                        NPC.velocity *= 0.8f;
+                    }
+                }
+                if (NPC.velocity.Length() > 12f)
+                {
+                    NPC.velocity = Vector2.Normalize(NPC.velocity) * 12f;
+                }
+            }
+            //无目标待机
+            else
+            {
+                MoveAroundPlayer(player);
+
+
+                //float speed = 0.2f;// 游荡速度系数
+                //float dis = player.Center.Distance(NPC.Center);// 弹幕到玩家距离，越近越慢
+                //if (dis < 200f)
+                //{
+                //    speed = 0.12f;
+                //}
+                //if (dis < 140f)
+                //{
+                //    speed = 0.06f;
+                //}
+                //if (dis > 100f)
+                //{
+                //    // abs绝对值，sign正负
+                //    float toPx = player.Center.X - NPC.Center.X;
+                //    float toPy = player.Center.Y - NPC.Center.Y;
+                //    if (Math.Abs(toPx) > 20f)// 离玩家水平距离大于20，就给朝向玩家的水平速度
+                //    {
+                //        NPC.velocity.X += speed * Math.Sign(toPx);
+                //    }
+                //    if (Math.Abs(toPy) > 8f)// 离玩家垂直距离大于10，就给朝向玩家的垂直速度
+                //    {
+                //        NPC.velocity.Y += speed * Math.Sign(toPy);
+                //    }
+                //}
+                //else if (NPC.velocity.Length() > 2f)// 在100f内且速度较大就减速
+                //{
+                //    NPC.velocity *= 0.9f;
+                //}
+                //if (Math.Abs(NPC.velocity.Y) < 1f)// 没啥速度了就慢慢向上游动
+                //{
+                //    NPC.velocity.Y -= 0.04f;
+                //}
+                //if (NPC.velocity.Length() > 8f)
+                //{
+                //    NPC.velocity = Vector2.Normalize(NPC.velocity) * 8f;
+                //}
+            }
+
+            base.AI();
+        }
+        public override void FindFrame(int frameHeight)
+        {
+            int count = 0;
+
+            if (c % 10 > 5)
+            {
+                count = 1;
+            }
+            if (NPC.velocity.X > 0f)
+            {
+                count += 2;
+            }
+            NPC.frame.Y = count * frameHeight;
+            base.FindFrame(frameHeight);
+        }
+        //public override void OnKill()
+        //{
+        //    Player player = Main.player[(int)NPC.ai[0]];
+        //    player.GetModPlayer<技能效果>().BatCount--;
+        //    base.OnKill();
+        //}
+    }
+}
---
+++ NPCs/鬼爵分身.cs
@@ -0,0 +1,168 @@
+﻿using System;
+
+namespace 爆枪英雄.NPCs
+{
+    public class 鬼爵分身 : ModNPC
+    {
+
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[NPC.type] = 4;
+            //NPC总共帧图数，一般为16+下面两种帧的帧数            
+        }
+        public override void SetDefaults()
+        {
+            NPC.friendly = true;
+            NPC.width = 36;
+            NPC.height = 63;
+            NPC.aiStyle = -1;
+            NPC.damage = 76;
+            NPC.defense = 100;
+            NPC.lifeMax = 5000;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+            NPC.knockBackResist = 0f;
+            NPC.npcSlots = 0f;
+        }
+        int attackWays = 0;
+        public override void AI()
+        {
+            NPC.ai[1]++;
+            Player Owner = Main.player[(int)NPC.ai[0]];
+            Owner.AddBuff(BuffType<分身>(), 60);
+            if (Owner.dead )
+            {
+                NPC.life = -1;
+            }
+            //
+            if (!NPC.WithinRange(Owner.Center, 1000f))
+            {
+                NPC.position = Owner.Center;
+                NPC.velocity *= 0.3f;
+                NPC.netUpdate = true;
+            }
+            //
+            int TarWho = SmMthd.GetTarWho(NPC.Center, this, 800f);
+            if (TarWho != -1)
+            {
+                NPC target = Main.npc[TarWho];
+                Vector2 dis = target.Center - NPC.Center;
+                float disY = Math.Abs(dis.Y);
+                float disX = Math.Abs(dis.X);
+                //
+                if (disY > 130f + target.Size.Length() / 2)
+                {
+                    NPC.velocity.Y += dis.Y / 20f;
+                }
+                else
+                {
+                    NPC.velocity.Y -= 0.5f;
+                    if (NPC.velocity.Length() > 1f)
+                    {
+                        NPC.velocity *= 0.9f;
+                    }
+                }
+
+                if (disX > 150f + target.Size.Length() / 2)
+                {
+                    NPC.velocity.X += dis.X / 25f;
+                }
+
+                if (NPC.velocity.Length() > 8f)
+                {
+                    NPC.velocity = Vector2.Normalize(NPC.velocity) * 8f;
+                }
+                //
+                if (NPC.ai[1] % 300 == 0)
+                {
+                    attackWays++;
+                    if (attackWays > 2)
+                    {
+                        attackWays = 0;
+                    }
+                }
+
+                if (attackWays == 0) JianChiShoot(dis);
+                else if (attackWays == 1) XunLongShoot(dis);
+                else if (attackWays == 2) ShaEShoot(dis);
+
+            }
+            else
+            {
+                if (NPC.WithinRange(Owner.Center, 1000f) && !NPC.WithinRange(NPC.Center, 300f))
+                {
+                    NPC.velocity = (Owner.Center - NPC.Center) / 10f;
+                }
+                else if (!NPC.WithinRange(Owner.Center, 160f))
+                {
+                    Vector2 vel = Owner.Center - NPC.Center;
+                    vel.Normalize();
+                    NPC.velocity = (NPC.velocity * 37f + vel * 17f) / 40f;
+                }
+                if ((NPC.Center.Y - Owner.Center.Y > 100f && NPC.ai[1] % 60 == 0))
+                {
+                    NPC.velocity.Y -= 10f;
+                }
+            }
+            //
+            base.AI();
+        }
+        public override void OnKill()
+        {
+            Player player = Main.player[(int)NPC.ai[0]];
+            player.GetModPlayer<某些装备效果>().分身冷却 = 120 * 60f;
+            base.OnKill();
+        }
+        public override void FindFrame(int frameHeight)
+        {
+            int count = 0;
+
+            if (NPC.ai[1] % 10 > 5)
+            {
+                count = 1;
+            }
+            if (NPC.velocity.X < 0f)
+            {
+                count += 2;
+            }
+            NPC.frame.Y = count * frameHeight;
+            base.FindFrame(frameHeight);
+        }
+        private void ShaEShoot(Vector2 dis)
+        {
+            if (NPC.ai[1] % 30 == 0 && dis.Length() <= 700f)
+            {
+                for (int i = 0; i < 11; i++)
+                {
+                    float randAngle = Main.rand.NextFloat(-0.32f, 0.33f);
+                    float randVelMultiplier = Main.rand.NextFloat(0.85f, 1.15f);
+                    Vector2 velocity = Vector2.Normalize(dis);
+                    Vector2 cwVelocity = velocity.RotatedBy(randAngle);
+                    Projectile.NewProjectile(NPC.GetSource_FromAI(), NPC.Center, 18f * cwVelocity * randVelMultiplier, ProjectileType<沙鳄弹幕>(), NPC.damage, 3, -1, NPC.whoAmI);
+                }
+            }
+        }
+        private void XunLongShoot(Vector2 dis)
+        {
+            if (NPC.ai[1] % 7 == 0 && dis.Length() <= 850f)
+            {
+                Projectile.NewProjectile(NPC.GetSource_FromAI(), NPC.Center, 18f * Vector2.Normalize(dis), ProjectileType<迅龙弹幕>(), NPC.damage, 3, -1, NPC.whoAmI);
+            }
+        }
+        private void JianChiShoot(Vector2 dis)
+        {
+            if (NPC.ai[1] % 55 == 0 && dis.Length() <= 1000f)
+            {
+                Projectile.NewProjectile(NPC.GetSource_FromAI(), NPC.Center, 18f * Vector2.Normalize(dis), ProjectileType<剑齿弹幕>(), NPC.damage, 3, -1, NPC.whoAmI);
+                for (int i = 0; i < 2; i++)
+                {
+                    float randAngle = Main.rand.NextFloat(-0.35f, 0.34f);
+                    float randVelMultiplier = Main.rand.NextFloat(0.7f, 1.3f);
+                    Vector2 velocity = Vector2.Normalize(dis);
+                    Vector2 cwVelocity = velocity.RotatedBy(randAngle);
+                    Projectile.NewProjectile(NPC.GetSource_FromAI(), NPC.Center, 18f * cwVelocity * randVelMultiplier, ProjectileType<影灭弹幕>(), NPC.damage * 2, 3, -1, NPC.whoAmI);
+                }
+            }
+        }
+    }
+}
